712d0389ad9e518d6933f8b1da3847c59098b05d
GreyStekl0 <gulev_s@list.ru>
2024-05-10 15:59:41 +0500

    finish?

--- Lab_10_var_29.c
+++ Lab_10_var_29.c
@@ -108,17 +108,28 @@ void save_processes_to_file(const char *filename, Process *processes, int curren
     printf("Processes have been successfully saved to file %s.\n", filename);
 }
 
-void input_process(Process *process, Process *processes, int current_size, bool is_new_process) {
-    if (is_new_process) {
-        int id;
-        do {
-            id = input_number("Enter process id: ", 0, INT_MAX);
-            if (!is_unique_id(processes, current_size, id)) {
-                printf("This id is already in use. Please enter a unique id.\n");
-            }
-        } while (!is_unique_id(processes, current_size, id));
-        process->id = id;
+bool realloc_processes(Process **processes, int *current_size, int *max_size) {
+    if (*current_size == *max_size) {
+        *max_size += 10;
+        Process *temp = realloc(*processes, *max_size * sizeof(Process));
+        if (temp == NULL) {
+            printf("Memory allocation failed. No changes were made.\n");
+            return false;
+        }
+        *processes = temp;
     }
+    return true;
+}
+
+void input_process(Process *process, Process **processes, int current_size) {
+    int id;
+    do {
+        id = input_number("Enter process id: ", 0, INT_MAX);
+        if (!is_unique_id(*processes, current_size, id)) {
+            printf("This id is already in use. Please enter a unique id.\n");
+        }
+    } while (!is_unique_id(*processes, current_size, id));
+    process->id = id;
     process->priority = input_number("Enter process priority (1-10): ", 1, 10);
     process->creation_time.hours = input_number("Enter process creation time (hours): ", 0, 23);
     process->creation_time.minutes = input_number("Enter process creation time (minutes): ", 0, 59);
@@ -128,42 +139,59 @@ void input_process(Process *process, Process *processes, int current_size, bool 
     process->open_resources = input_number("Enter process open resources: ", 0, INT_MAX);
 }
 
-void delete_process(Process *processes, int *current_size, int id) {
+void add_process(Process **processes, int *current_size, int *max_size) {
+    if (!realloc_processes(processes, current_size, max_size)) {
+        printf("Failed to add process due to memory allocation error.\n");
+        return;
+    }
+    input_process(processes, current_size, *max_size);
+}
+
+void delete_process(Process **processes, int *current_size, int *max_size, int id) {
     int i;
     for (i = 0; i < *current_size; i++) {
-        if (processes[i].id == id) {
+        if ((*processes)[i].id == id) {
             break;
         }
     }
     if (i != *current_size) {
         int j;
         for (j = i; j < *current_size - 1; j++) {
-            processes[j] = processes[j + 1];
+            (*processes)[j] = (*processes)[j + 1];
         }
         (*current_size)--;
         printf("Process with id %d has been deleted.\n", id);
+
+        if (*current_size < *max_size / 2) {
+            *max_size /= 2;
+            Process *temp = realloc(*processes, *max_size * sizeof(Process));
+            if (temp == NULL) {
+                printf("Memory allocation failed. No changes were made.\n");
+                return;
+            }
+            *processes = temp;
+        }
     } else {
         printf("Process with id %d not found.\n", id);
     }
 }
 
-void insert_process(Process *processes, int *current_size, int max_size) {
-    if (*current_size == max_size) {
-        printf("No more space for new processes.\n");
-        return;
-    }
-
+void insert_process(Process **processes, int *current_size, int *max_size) {
     int position;
     do {
         position = input_number("Enter the position where you want to insert the new process (0 to n): ", 0, *current_size);
     } while (position < 0 || position > *current_size);
 
-    for (int i = *current_size; i > position; i--) {
-        processes[i] = processes[i - 1];
+    if (!realloc_processes(processes, current_size, max_size)) {
+        printf("Failed to input process due to memory allocation error.\n");
+        return;
     }
 
-    input_process(&processes[position], processes, *current_size, true);
-    (*current_size)++;
+    for (int i = *current_size; i > position; i--) {
+        (*processes)[i] = (*processes)[i - 1];
+    }
+
+    input_process(processes, current_size, *max_size);
 }
 
 void load_processes_from_file(const char *filename, Process **processes, int *current_size, int *max_size) {
@@ -178,14 +206,15 @@ void load_processes_from_file(const char *filename, Process **processes, int *cu
         printf("Failed to open file %s\n", filename);
         return;
     }
+    *current_size = 0;
 
     Process process;
     while (fscanf(file, "%d %d %d %d %d %d %d %d", &process.id, &process.priority,
                   &process.creation_time.hours, &process.creation_time.minutes, &process.creation_time.seconds,
                   &process.available_memory, &process.occupied_memory, &process.open_resources) == 8) {
-        if (*current_size == *max_size) {
-            *max_size = (*max_size > 0) ? *max_size * 2 : 1;
-            *processes = realloc(*processes, *max_size * sizeof(Process));
+        if (!realloc_processes(processes, current_size, max_size)) {
+            printf("Failed to load processes from file due to memory allocation error.\n");
+            return;
         }
         (*processes)[(*current_size)++] = process;
     }
@@ -194,8 +223,11 @@ void load_processes_from_file(const char *filename, Process **processes, int *cu
     printf("File %s has been successfully read.\n", filename);
 }
 
-void reset_processes(Process *processes, int *current_size) {
+void reset_processes(Process **processes, int *current_size, int *max_size) {
     *current_size = 0;
+    *max_size = 0;
+    free(*processes);
+    *processes = NULL;
     printf("All processes have been reset.\n");
 }
 
@@ -233,27 +265,18 @@ int main(void) {
                 load_processes_from_file(filename, &processes, &current_size, &max_size);
                 break;
             case 2:
-                if (current_size == max_size) {
-                    max_size *= 2;
-                    processes = realloc(processes, max_size * sizeof(Process));
-                }
-                input_process(&processes[current_size], processes, current_size, true);
-                current_size++;
+                add_process(&processes, &current_size, &max_size);
                 display_processes(processes, current_size);
                 break;
             case 3:
                 printf("Enter the id of the process you want to delete: ");
                 int id_to_delete;
                 scanf("%d", &id_to_delete);
-                delete_process(processes, &current_size, id_to_delete);
+                delete_process(&processes, &current_size, &max_size, id_to_delete);
                 display_processes(processes, current_size);
-                if (current_size < max_size / 2) {
-                    max_size /= 2;
-                    processes = realloc(processes, max_size * sizeof(Process));
-                }
                 break;
             case 4:
-                insert_process(processes, &current_size, max_size);
+                insert_process(processes, &current_size, &max_size);
                 display_processes(processes, current_size);
                 break;
             case 5:
@@ -279,9 +302,7 @@ int main(void) {
                 display_processes(processes, current_size);
                 break;
             case 10:
-                reset_processes(processes, &current_size);
-                processes = realloc(processes, sizeof(Process));
-                max_size = 1;
+                reset_processes(&processes, &current_size, &max_size);
                 break;
             case 11:
                 printf("Enter the name of the file you want to save processes to: ");
